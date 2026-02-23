c01a65458c482edf2d6d95fe6d76f6067afdbe8b
Shane <shane@equipscout.com>
2026-02-23 10:11:56 -0600

    Add Sentry error tracking

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -1,3 +1,10 @@
+import * as Sentry from "@sentry/browser";
+
+Sentry.init({
+  dsn: "https://6130519f312d21103c2b234753860df2@o4510936111644672.ingest.us.sentry.io/4510936119050240",
+  tracesSampleRate: 0.1,
+});
+
 import { createRoot } from "react-dom/client";
 import App from "./App.js";
 import "./index.css";
