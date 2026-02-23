8ad91cc1066da9bc68f9e9b1cef045093edf5130
Shane <shane@equipscout.com>
2026-02-23 10:37:30 -0600

    Fix Sentry init order in frontend entry

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -1,12 +1,11 @@
 import * as Sentry from "@sentry/browser";
+import { createRoot } from "react-dom/client";
+import App from "./App.js";
+import "./index.css";
 
 Sentry.init({
   dsn: "https://6130519f312d21103c2b234753860df2@o4510936111644672.ingest.us.sentry.io/4510936119050240",
   tracesSampleRate: 0.1,
 });
-
-import { createRoot } from "react-dom/client";
-import App from "./App.js";
-import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
